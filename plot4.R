197a559187f4a132011bb9eb05da91a4793c48d0
Seananigans <sph99@comcast.net>
2015-01-11 10:24:42 -0500

    Assignment 1 Plot functions edited

--- plot4.R
+++ plot4.R
@@ -1,17 +1,20 @@
-setwd("H:/My Documents/ExploreData/Project1")
+#Set your working directory with the Dataset
+setwd("H:/My Documents/ExploreData")
 
-HouseData <- read.table("H:/My Documents/ExploreData/household_power_consumption/household_power_consumption.txt", header= TRUE, sep = ";")
+HouseData <- read.table("./household_power_consumption/household_power_consumption.txt", 
+                        header= TRUE, sep = ";")
 HouseData$Date <- as.Date(HouseData$Date, format = "%d/%m/%Y")
-HouseData <- HouseData[HouseData$Date>="2007-02-01"&HouseData$Date<="2007-02-02",]
+HouseData <- HouseData[HouseData$Date>="2007-02-01"& 
+                               HouseData$Date<="2007-02-02",]
 DateTime <- as.POSIXct(paste(HouseData$Date,HouseData$Time))
 HouseData$DateTime <- DateTime
 HouseData$DateTime <- strptime(HouseData$DateTime, format = "%Y-%m-%d %H:%M:%S")
 HouseData <- HouseData[,3:10]
 for(i in 1:7) {
-  HouseData[,i] <- as.numeric(as.character(HouseData[,i]))
+        HouseData[,i] <- as.numeric(as.character(HouseData[,i]))
 }
 
-
+# Create a new Dataset with the necessar values for this analysis
 #write.table(HouseData, "HousePowerConsumption.txt",sep = "\t")
 #HouseData <- read.table("HousePowerConsumption.txt",sep = "\t", header = TRUE)
 
